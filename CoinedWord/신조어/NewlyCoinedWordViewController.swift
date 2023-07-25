48c7424e4b5f67a6285f06724bae547bd2c73520
SeungwooSeo <prodvssw.98@gmail.com>
2023-07-25 18:26:38 +0900

    신조어 검색기 딕셔너리 뿐만 아니라 열거형도 사용할 수 있게 변경

--- CoinedWord/신조어/NewlyCoinedWordViewController.swift
+++ CoinedWord/신조어/NewlyCoinedWordViewController.swift
@@ -9,6 +9,34 @@ import UIKit
 
 final class NewlyCoinedWordViewController: UIViewController {
 
+    enum NewlyConinedWordKind: String, CaseIterable {
+        case 쫌쫌따리 = "아주 조금씩 하찮은 양을 모으는 모습"
+        case 억텐 = "억지 텐션의 줄임말. 억지로 텐션을 올려서 발랄하게 행동할 때"
+        case 스불재 = "스스로 불러온 재앙의 줄임말. 자신이 계획한 일로 자신이 고통을 받을 때 씀"
+        case 좋댓구알 = "좋아요, 댓글, 구독, 알림 설정 유튜브에서 동영상 보는 사람들이 할 수 있는 네 가지"
+        case 어쩔티비 = "어쩌라고 가서 TV나 봐라라는 뜻. ‘저쩔티비’나 ‘어쩔’ 뒤에 전자제품을 붙여 맞받아치면 됨. Ex) 어쩔냉장고"
+        case 갓생 = "갓(God) + 생(生) 부지런히하고 열심히 사는 사람에게 쓰는 말"
+        case 점메추 = "점심 메뉴 추천의 줄임말"
+        case 다꾸 = "다이어리 꾸미기라는 뜻"
+        case 킹받다 = "열받다라는 말에 새롭게 접두사로 쓰이고 있는 킹(King)을 붙여 강조"
+        case 군싹 = "군침이 싹도네의 줄임말"
+
+        var title: String {
+            switch self {
+            case .쫌쫌따리: return "쫌쫌따리"
+            case .억텐: return "억텐"
+            case .스불재: return "스불재"
+            case .좋댓구알: return "좋댓구알"
+            case .어쩔티비: return "어쩔티비"
+            case .갓생: return "갓생"
+            case .점메추: return "점메추"
+            case .다꾸: return "다꾸"
+            case .킹받다: return "킹받다"
+            case .군싹: return "군싹"
+            }
+        }
+    }
+
     let newlyCoinedWordDic: [String: String] = [
         "쫌쫌따리": "아주 조금씩 하찮은 양을 모으는 모습",
         "억텐": "억지 텐션의 줄임말. 억지로 텐션을 올려서 발랄하게 행동할 때",
@@ -22,15 +50,23 @@ final class NewlyCoinedWordViewController: UIViewController {
         "군싹": "군침이 싹도네의 줄임말"
     ]
 
+    // 검색 바
     @IBOutlet weak var searchBar: UISearchBar!
+    // 검색 버튼
     @IBOutlet weak var searchButton: UIButton!
 
+    // 키워드 스택뷰
     @IBOutlet weak var keywordStackView: UIStackView!
+    // 키워드 버튼들
     @IBOutlet var keywordButtons: [AutoAddPaddingButtton]!
 
+    // 신조어 스택뷰
     @IBOutlet weak var newlyCoinedWordStackView: UIStackView!
+    // 신조어 헤더뷰
     @IBOutlet weak var newlyCoinedWordHeaderView: UIView!
+    // 신조어 레이블
     @IBOutlet weak var newlyCoinedWordLabel: UILabel!
+    // 신조어 푸터뷰
     @IBOutlet weak var newlyCoinedWordFooterView: UIView!
 
     override func viewDidLoad() {
@@ -57,8 +93,7 @@ final class NewlyCoinedWordViewController: UIViewController {
         }
 
         newlyCoinedWordSearch(searchText: text)
-
-        randomizationKeywordStackView()
+        randomizationKeywordStackViewFlow()
     }
 
     @IBAction func dismissKeyboard(_ sender: Any) {
@@ -82,32 +117,99 @@ extension NewlyCoinedWordViewController: UITextFieldDelegate {
 
 }
 
+// UI 디폴트 설정
 private extension NewlyCoinedWordViewController {
 
-    func newlyCoinedWordSearch(searchText: String) {
-        // 공백만 있어도 isEmpty 속성이 통하지 않기 때문에
-        // 공백을 제거해준다.
-        let searchText = searchText.trimmingCharacters(in: .whitespaces)
+    // 검색 바 구성하기
+    func configureSearchBar() {
+        searchBar.searchTextField.delegate = self
+        searchBar.setImage(
+            UIImage(),
+            for: UISearchBar.Icon.search,
+            state: .normal
+        )
+        searchBar.searchTextField.backgroundColor = UIColor.clear
+        searchBar.backgroundImage = UIImage()
 
-        // 검색어가 비었을 때
-        if searchText.isEmpty {
-            presentAlert(title: "검색어를 입력하세요.")
-        // 검색어도 있고 신조어도 있을 때
-        } else if newlyCoinedWordDic.keys.contains(searchText) {
-            searchBar.searchTextField.text = nil
-            newlyCoinedWordLabel.text = newlyCoinedWordDic[searchText]
-            searchBar.searchTextField.resignFirstResponder()
-        // 검색어가 비어있지 않지만 없을 때
+        drawBorder(
+            view: searchBar,
+            width: 3
+        )
+    }
+
+    // 검색 버튼 구성하기
+    func configureSearchButton() {
+        if #available(iOS 15.0, *) {
+            var config = UIButton.Configuration.plain()
+            config.image = UIImage(
+                systemName: "magnifyingglass"
+            )
+            config.baseForegroundColor = .white
+            config.background.backgroundColor = .black
+            config.background.cornerRadius = 0
+
+            searchButton.configuration = config
         } else {
-            presentAlert(title: "없는 검색어입니다.")
+            searchButton.setImage(
+                UIImage(systemName: "magnifyingglass"),
+                for: .normal
+            )
+            searchButton.tintColor = .white
+            searchButton.backgroundColor = .black
         }
+    }
+
+    // 키워드 버튼들 구성하기
+    func configureKeywordButtons() {
+        keywordButtons.forEach {
+            $0.layer.cornerRadius = 10
+            drawBorder(view: $0, width: 1.5)
+        }
+    }
+
+    // 신조어 스택뷰 구성하기
+    func configureNewlyCoinedWordStackView() {
+        drawBorder(
+            view: newlyCoinedWordStackView,
+            width: 2
+        )
+    }
+
+    // 신조어 레이블 구성하기
+    func configureNewlyCoinedWordLabel() {
+        newlyCoinedWordLabel.text = nil
+        newlyCoinedWordLabel.font = .systemFont(
+            ofSize: 24,
+            weight: .bold
+        )
+        newlyCoinedWordLabel.textAlignment = .center
+        newlyCoinedWordLabel.numberOfLines = 4
+
+        drawBorder(
+            view: newlyCoinedWordLabel,
+            width: 2
+        )
+    }
+
+    func configureHierarchy() {
+        // 검색 바
+        configureSearchBar()
+        // 검색 버튼
+        configureSearchButton()
+        // 키워드 버튼들
+        configureKeywordButtons()
+        // 신조어 스택뷰
+        configureNewlyCoinedWordStackView()
+        // 신조어 레이블
+        configureNewlyCoinedWordLabel()
     }
 
 }
 
+// 동작
 private extension NewlyCoinedWordViewController {
 
-    // border 그리는 메소드
+    // border 그려주기
     func drawBorder(
         view: UIView,
         width: CGFloat,
@@ -136,114 +238,40 @@ private extension NewlyCoinedWordViewController {
 
 }
 
-// UI 설정
+// 비즈니스 로직
+// 검색
 private extension NewlyCoinedWordViewController {
 
-    func configureHierarchy() {
-        // 검색 바
-        configureSearchBar()
-        // 검색 버튼
-        configureSearchButton()
-        // 키워드 버튼들
-        configureKeywordButtons()
-        // 신조어 스택뷰
-        configureNewlyCoinedWordStackView()
-        // 신조어 레이블
-        configureNewlyCoinedWordLabel()
-    }
-
-    func configureSearchBar() {
-        searchBar.searchTextField.delegate = self
-        searchBar.setImage(
-            UIImage(),
-            for: UISearchBar.Icon.search,
-            state: .normal
+    // 신조어 검색하기
+    func newlyCoinedWordSearch(searchText: String) {
+        // 공백만 있어도 isEmpty 속성이 통하지 않기 때문에
+        // 공백을 제거해준다.
+        let searchText = searchText.trimmingCharacters(
+            in: .whitespaces
         )
-        searchBar.searchTextField.backgroundColor = UIColor.clear
-        searchBar.backgroundImage = UIImage()
 
-        drawBorder(
-            view: searchBar,
-            width: 3
-        )
-    }
-
-    func configureSearchButton() {
-        if #available(iOS 15.0, *) {
-            var config = UIButton.Configuration.plain()
-            config.image = UIImage(
-                systemName: "magnifyingglass"
-            )
-            config.baseForegroundColor = .white
-            config.background.backgroundColor = .black
-            config.background.cornerRadius = 0
-
-            searchButton.configuration = config
+        // 검색어가 비었을 때
+        if searchText.isEmpty {
+            presentAlert(title: "검색어를 입력하세요.")
+            // 검색어도 있고 신조어도 있을 때
+        } else if newlyCoinedWordDic.keys.contains(searchText) {
+            searchBar.searchTextField.text = nil
+            newlyCoinedWordLabel.text = newlyCoinedWordDic[searchText]
+            searchBar.searchTextField.resignFirstResponder()
+            // 검색어가 비어있지 않지만 없을 때
         } else {
-            searchButton.setImage(
-                UIImage(systemName: "magnifyingglass"),
-                for: .normal
-            )
-            searchButton.tintColor = .white
-            searchButton.backgroundColor = .black
+            presentAlert(title: "없는 검색어입니다.")
         }
-    }
-
-    func configureKeywordButtons() {
-        keywordButtons.forEach {
-            $0.layer.cornerRadius = 10
-            drawBorder(view: $0, width: 1.5)
-        }
-    }
-
-    func configureNewlyCoinedWordStackView() {
-        drawBorder(
-            view: newlyCoinedWordStackView,
-            width: 2
-        )
-    }
-
-    func configureNewlyCoinedWordLabel() {
-        newlyCoinedWordLabel.text = nil
-        newlyCoinedWordLabel.font = .systemFont(
-            ofSize: 24,
-            weight: .bold
-        )
-        newlyCoinedWordLabel.textAlignment = .center
-        newlyCoinedWordLabel.numberOfLines = 4
-
-        drawBorder(
-            view: newlyCoinedWordLabel,
-            width: 2
-        )
     }
 
 }
 
-// 비즈니스 로직?
+// 랜덤 구성
 private extension NewlyCoinedWordViewController {
 
-    // keywordStackView를 랜덤으로 구성하기
-    func randomizationKeywordStackView() {
-        // 랜덤으로 가져온 신조어들
-        let randomNewlyConinedWords = getRandomNewlyConinedWords()
-        // 랜덤으로 가져온 신조어의 갯수
-        let count = randomNewlyConinedWords.count
-
-        // 랜덤으로 가져온 신조어 갯수만큼만 hidden을 false로
-        for i in (0...count-1) {
-            keywordButtons[i].isHidden = false
-            keywordButtons[i].titleLabel?.text = randomNewlyConinedWords[i]
-        }
-
-        // 이후 버튼들은 false로
-        for button in keywordButtons.suffix(from: count) {
-            button.isHidden = true
-        }
-    }
-
     // 랜덤으로 신조어들을 가져오기
-    func getRandomNewlyConinedWords() -> [String] {
+    // 1. 딕셔너리에서 가져올 때
+    var getRandomNewlyConinedWordsAtDictionary: [String] {
         // 신조어 딕셔너리에서 키값만 뽑기
         let newlyConinedWords = newlyCoinedWordDic.keys.map { $0 }
 
@@ -263,6 +291,83 @@ private extension NewlyCoinedWordViewController {
         let result = randomNewlyConinedWords.compactMap { $0 }
 
         return result
+    }
+
+    // 2. 열거형에서 가져올 때
+    var getRandomNewlyConinedWordsAtEnum: [NewlyConinedWordKind] {
+        let newlyConinedWords = NewlyConinedWordKind.allCases
+
+        var randomNewlyConinedWords: Set<NewlyConinedWordKind?> = []
+
+        for _ in 0...4 {
+            randomNewlyConinedWords.insert(
+                newlyConinedWords.randomElement()
+            )
+        }
+
+        let result = randomNewlyConinedWords.compactMap { $0 }
+
+        return result
+    }
+
+    // keywordStackView를 랜덤으로 구성하기
+    func randomizationKeywordStackView<T: Sequence>(
+        sequence: T
+    ) {
+        let count: Int
+
+        if let sequence = sequence as? [String] {
+            // 랜덤으로 가져온 신조어 갯수만큼만 hidden을 false로
+            count = sequence.count
+
+            for i in (0...count-1) {
+                keywordButtons[i].isHidden = false
+                keywordButtons[i].setTitle(
+                    sequence[i],
+                    for: .normal
+                )
+            }
+
+            // 이후 버튼들은 false로
+            for button in keywordButtons.suffix(from: count) {
+                button.isHidden = true
+            }
+        } else if let sequence = sequence as? [NewlyConinedWordKind] {
+            count = sequence.count
+
+            // 랜덤으로 가져온 신조어 갯수만큼만 hidden을 false로
+            for i in (0...count-1) {
+                keywordButtons[i].isHidden = false
+                keywordButtons[i].setTitle(
+                    sequence[i].title,
+                    for: .normal
+                )
+            }
+
+            // 이후 버튼들은 false로
+            for button in keywordButtons.suffix(from: count) {
+                button.isHidden = true
+            }
+        } else {
+            presentAlert(
+                title: "딕셔너리 배열도 아니고 열거형 배열도 아닙니다."
+            )
+        }
+    }
+
+    // keywordStackView를 랜덤으로 구성하는 흐름
+    func randomizationKeywordStackViewFlow(flag: Bool = false) {
+        let sequence: any Sequence
+
+        if flag {
+            // 딕셔너리 값 사용하고 싶을 때
+            sequence = getRandomNewlyConinedWordsAtDictionary
+        } else {
+            // 열거형 값 사용하고 싶을 때
+            sequence = getRandomNewlyConinedWordsAtEnum
+        }
+
+        randomizationKeywordStackView(sequence: sequence)
     }
 
 }
